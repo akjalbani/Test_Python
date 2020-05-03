9a8ff029a83aa852376a6d20b574c411015f5071
akjalbani <akjalbani@yahoo.com>
2020-05-03 18:57:24 +1000

    Create Session1_1.py

---
+++ Session1_1.py
@@ -0,0 +1,8 @@
+#############################################
+# Q1: display your date of birth
+#dateOfBirth = "PUT_YOUR_BIRTHDAY_HERE"
+#print("My birthday is " + dateOfBirth)
+##############################################
+dateOfBirth= "22-04-1990"
+print("My birthday is " + dateOfBirth)
+
