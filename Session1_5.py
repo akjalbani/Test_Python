20dadf18b588201028922f05528861553196492b
akjalbani <akjalbani@yahoo.com>
2020-05-03 18:59:17 +1000

    Create Session1_5.py

---
+++ Session1_5.py
@@ -0,0 +1,6 @@
+## Q5
+name = input("Enter Name: ")
+dateOfBirth = input("Enter your DOB: (dd-mm-yy): ")
+email = input("Enter your email: ")
+# dispaly on online
+print(name+" "+dateOfBirth+ " "+email)
