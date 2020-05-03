3ed7ecbc117685451531dc2f13f1fbec6d360515
akjalbani <akjalbani@yahoo.com>
2020-05-03 18:58:40 +1000

    Create Session1_4.py

---
+++ Session1_4.py
@@ -0,0 +1,27 @@
+#################################
+##4 Declare two variables a=15 and b=8.Perform all the ###arithmetic operations on them like :
+##C=a+b and print the results(print(c)).
+##################################
+a = 15
+b = 8
+# addition
+c = a+b
+print("a+b= ",c)
+# subtraction
+c = a - b
+print("a-b= ",c)
+# Multiplication
+c = a * b
+print("a*b= ",c)
+# division
+c = a /b
+print("a/b= ",c)
+# exponenet
+c = a ** b
+print("a**b= ",c)
+#floor division
+c = a // b
+print("a//b= ",c)
+#mod
+c = a % b
+print("a%b= ",c)
