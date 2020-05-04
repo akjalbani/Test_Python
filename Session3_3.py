e6107aef3f0db7e0efe7c4de8c40078f90130c55
akjalbani <akjalbani@yahoo.com>
2020-05-04 23:17:25 +1000

    Update Session3_3.py

--- Session3_3.py
+++ Session3_3.py
@@ -12,3 +12,18 @@ else:
   print("Last two digits of {} is {}".format(number, last_two_digit))
 
   
+# With Error Handling 
+try:
+  number = input("Enter 3 digit number ->")
+  if len(number) <3 or len(number) > 3:
+    print("{} is not a 3 digit number".format(number))
+  else:
+    number = int(number) # convert it into integer bwfore processing
+    right_most_digit= number % 10  # rght most digit
+    print("Right most digit of {} is {}".format(number, right_most_digit))
+
+    last_two_digit= number % 100  #  last_two_digit
+    print("Last two digits of {} is {}".format(number, last_two_digit))
+except ValueError:
+  print("Not a Valid Input")
+  
