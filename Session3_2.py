7cf14797aceb654baf1d0dbddce530e1db99e860
akjalbani <akjalbani@yahoo.com>
2020-05-04 23:09:17 +1000

    Update Session3_2.py
    added exception handling

--- Session3_2.py
+++ Session3_2.py
@@ -8,3 +8,16 @@ else:
   print("Sorry {} is not divisible by {}.".format(a,b))
 
   
+#### This can be improved by handling errors 
+
+try:
+  a = int(input("Enter value of a -> " ))
+  b = int(input("Enter value of b -> " ))
+  if a % b == 0:
+    print("{} is divisible by {}.".format(a,b))
+  else:
+    print("Sorry {} is not divisible by {}.".format(a,b))
+except ValueError:
+  print("Sorry, Invlaid input")
+except ZeroDivisionError:
+  print("Sorry!! cannot be divided by zero")
