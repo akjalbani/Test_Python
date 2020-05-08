4410f2bdcc4d51ccc56321c13bca80c07b633e86
akjalbani <akjalbani@yahoo.com>
2020-05-08 20:20:12 +1000

    Update Session10_ex4.py

--- Session10_ex4.py
+++ Session10_ex4.py
@@ -3,7 +3,11 @@ class ReverseString:
   def __init__(self,mystring):
     self.mystring= mystring
   def reverse_string(self):
-    return ' '.join(self.mystring.split(' ')[::-1])  
+    #return ' '.join(self.mystring.split(' ')[::-1])  # this is also one way
+    # another way
+    words = self.mystring.split() 
+    words = list(reversed(words))
+    return " ".join(words)
 
 user_message = input("Type here ===> ")
 
