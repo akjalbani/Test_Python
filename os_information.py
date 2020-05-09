70319520e63ef42533a655c2178f2909db1f78ab
akjalbani <akjalbani@yahoo.com>
2020-05-10 00:13:16 +1000

    Create os_information.py

---
+++ os_information.py
@@ -0,0 +1,16 @@
+import os,platform,sys,sysconfig
+
+# print(os. getcwd()) 
+# print(os.name)
+# print(os.uname())
+# print(sys.platform)
+# print(platform.system())
+
+print("os.name                      ",  os.name)
+print("sys.platform                 ",  sys.platform)
+print("platform.system()            ",  platform.system())
+print("sysconfig.get_platform()     ",  sysconfig.get_platform())
+print("platform.machine()           ",  platform.machine())
+print("platform.architecture()      ",  platform.architecture())
+
+
