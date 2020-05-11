7be16638998adc27aa5cfae0a09a019a74b3aad1
akjalbani <akjalbani@yahoo.com>
2020-05-11 16:49:32 +1000

    Create Session4_ex2.py

---
+++ Session4_ex2.py
@@ -0,0 +1,6 @@
+'''2.	Write a Python program that prints all the numbers from 0 to 6 except 3 and 6.'''
+
+for i in range(0,7):
+  if i == 3 or i == 6:
+    continue
+  print("{} ".format(i), end="")
