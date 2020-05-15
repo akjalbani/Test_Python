8474ccfe4253a3364e83d1ab48f00c822b9f4693
akjalbani <akjalbani@yahoo.com>
2020-05-15 23:52:28 +1000

    Delete Session1_5.py
    moved to folder

--- Session1_5.py
+++
@@ -1,6 +0,0 @@
-## Q5
-name = input("Enter Name: ")
-dateOfBirth = input("Enter your DOB: (dd-mm-yy): ")
-email = input("Enter your email: ")
-# dispaly on online
-print(name+" "+dateOfBirth+ " "+email)
