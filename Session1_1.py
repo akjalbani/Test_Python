28176c1da53fa9cc9c28c10bc0d8a65cf57282e8
akjalbani <akjalbani@yahoo.com>
2020-05-15 23:51:41 +1000

    Delete Session1_1.py
    moved to folder

--- Session1_1.py
+++
@@ -1,8 +0,0 @@
-#############################################
-# Q1: display your date of birth
-#dateOfBirth = "PUT_YOUR_BIRTHDAY_HERE"
-#print("My birthday is " + dateOfBirth)
-##############################################
-dateOfBirth= "22-04-1990"
-print("My birthday is " + dateOfBirth)
-
