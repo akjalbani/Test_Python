505601bd246db9ec4f1a5c1465595ddbde2e1030
akjalbani <akjalbani@yahoo.com>
2020-05-15 23:36:17 +1000

    Add files via upload
    folder added

---
+++ Session1/Session1_2.py
@@ -0,0 +1,6 @@
+#Q2. Write a script that reads two numbers and prints #their sum.
+####################################################
+a = int(input("a -> "))
+b = int(input("b -> "))
+print(a+b)
+
---
+++ Session1/Session1_3.py
@@ -0,0 +1,7 @@
+#Q3. extend q2 and print sum of three numbers
+####################################################
+a = int(input("a -> "))
+b = int(input("b -> "))
+c = int(input("c -> "))
+print(a+b+c)
+
