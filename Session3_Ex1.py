aaf54c76fa2a5e87abf77ba27c9674c6a58d2087
akjalbani <akjalbani@yahoo.com>
2020-05-15 23:54:34 +1000

    Delete Session3_Ex1.py
    moved to folder

--- Session3_Ex1.py
+++
@@ -1,9 +0,0 @@
-'''Ex3.1 Write a python program to read the values of length and breadth of a rectangle from the user and check if it is square or not.'''
-
-length = int(input("Enter length of Rectangle-> "))
-breadth = int(input("Enter breadth of Rectangle ->"))
-# for square both sides must be equal
-if length==breadth:
-  print("It is a Square")
-else:
-  print("Sorry! it is not Square")
