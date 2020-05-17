8c5f9f5737f7da31f3e5af834adc347534c6fd40
akjalbani <akjalbani@yahoo.com>
2020-05-17 21:25:44 +1000

    Create square2.py
    function without parameters

---
+++ Misc/Turtle_examples/square2.py
@@ -0,0 +1,19 @@
+# This is simple square to demonstrate the function
+#@ author: Akhtar Jalbani
+
+import turtle
+turtle = turtle.Turtle()
+# function defination
+def square():
+  turtle.forward(100)
+  turtle.left(90)
+  turtle.forward(100)
+  turtle.left(90)
+  turtle.forward(100)
+  turtle.left(90)
+  turtle.forward(100)
+  turtle.left(90)
+
+# function call
+square()
+
