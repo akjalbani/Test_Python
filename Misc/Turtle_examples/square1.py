238e80907f9b2c726d704dac64538582d236e0d7
akjalbani <akjalbani@yahoo.com>
2020-05-17 21:23:40 +1000

    Create square1.py
    added

---
+++ Misc/Turtle_examples/square1.py
@@ -0,0 +1,18 @@
+# simple square with all steps
+# @ auhtor: Akhtar Jalbani
+
+import turtle
+turtle = turtle.Turtle()
+
+turtle.forward(100)
+turtle.left(90)
+
+turtle.forward(100)
+turtle.left(90)
+
+turtle.forward(100)
+turtle.left(90)
+
+turtle.forward(100)
+turtle.left(90)
+
