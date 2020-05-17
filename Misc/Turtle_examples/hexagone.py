848a3f646c7119285ae3f36c2234faec2fa4df7a
akjalbani <akjalbani@yahoo.com>
2020-05-17 21:35:10 +1000

    Create hexagone.py
    hexagone added

---
+++ Misc/Turtle_examples/hexagone.py
@@ -0,0 +1,8 @@
+# this is simple program to demostrate the for loop
+# @author: Akhtar Jalbani
+
+import turtle
+turtle = turtle.Turtle()
+for i in range(6):
+  turtle.forward(100)
+  turtle.left(60)
