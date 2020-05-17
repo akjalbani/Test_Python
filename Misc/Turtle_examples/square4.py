23a36c9c6532434853885c9a86c279cbe81a407a
akjalbani <akjalbani@yahoo.com>
2020-05-17 21:31:49 +1000

    Create square4.py
    square with for loop

---
+++ Misc/Turtle_examples/square4.py
@@ -0,0 +1,8 @@
+# This is simple square to demonstrate the for loop
+# @author: Akhtar Jalbani
+import turtle
+turtle = turtle.Turtle()
+for i in range(4):
+  turtle.forward(100)
+  turtle.left(90)
+ 
