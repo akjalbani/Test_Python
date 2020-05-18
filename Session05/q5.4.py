d5cbd914cceb8e74ac0edf5ea546f21353d527c4
akjalbani <akjalbani@yahoo.com>
2020-05-18 20:38:52 +1000

    Create q5.4.py
    added

---
+++ Session05/q5.4.py
@@ -0,0 +1,11 @@
+'''Q4
+numbers = [1,2,3,4,5,6,7,8,9]
+even_count = 0
+odd_count = 0
+for i in numbers:
+    if i%2 == 0:
+        even_count = even_count + 1
+    else:
+        odd_count = odd_count+1
+print("Total no. of Even numbers in the list = ",even_count)
+print("Total no. of Odd numbers in the list = ",odd_count)
