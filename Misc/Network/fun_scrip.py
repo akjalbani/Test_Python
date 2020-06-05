e177a251af91c4bea27ffd9480fe62acc7b1c507
akjalbani <akjalbani@yahoo.com>
2020-06-05 15:59:17 +1000

    Create fun_scrip.py

---
+++ Misc/Network/fun_scrip.py
@@ -0,0 +1,19 @@
+# you should run this script usig python Desktop IDE, online Editor repl.it may not support
+# As loop is indefinte, you need forecfully close the python running script.
+# Enjoy the script and have fun with the friends.
+# This script is only for the learning purpose and expecting same from you to utilize the script for learing purpose not to harm others.
+# author A.A Jalbani
+# version 1.0
+# Dated: 05/06/2020
+
+import webbrowser
+import time
+import random
+
+while True:
+    sites = random.choice(["google.com","yahoo.com","bbc.com"])
+    url ="http://{}".format(sites)
+    webbrowser.open(url)
+    seconds = random.randrange(5,20)
+    time.sleep(seconds)
+  
