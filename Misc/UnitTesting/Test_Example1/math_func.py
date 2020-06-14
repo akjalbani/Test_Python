cf1580c72d559b472cce309a95927c49039d4074
akjalbani <akjalbani@yahoo.com>
2020-06-14 18:30:07 +1000

    Create math_func.py

---
+++ Misc/UnitTesting/Test_Example1/math_func.py
@@ -0,0 +1,8 @@
+# This contians all your units/ function in your main file
+# let us say we have two functions in our python file
+
+def add(x,y=2):
+    return x+y
+
+def product(x,y=2):
+    return x*y
