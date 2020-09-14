693203ba4e387e8193bb84c235bcea89f31d72ae
akjalbani <akjalbani@yahoo.com>
2020-09-14 20:51:42 +1000

    Create Q3.py

---
+++ Session09/Q3.py
@@ -0,0 +1,8 @@
+''' Q3
+Write a Python script that takes input from the user and displays that input back in upper and lower cases.
+'''
+usr_input = input("Enter String: ")
+lower_case = usr_input.lower()
+upper_case = usr_input.upper()
+print(lower_case)
+print(upper_case)
