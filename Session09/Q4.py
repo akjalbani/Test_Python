874fba60927ed7840b1b22382cbe27bee672c723
akjalbani <akjalbani@yahoo.com>
2020-09-17 16:52:02 +1000

    Update Q4.py

--- Session09/Q4.py
+++ Session09/Q4.py
@@ -13,3 +13,22 @@ for word in words:
     count[word] = 1  # else put every thing in the new dictionary
 for k,v in count.items():
   print(k, "appears" ,v, "times")
+
+  
+  ##################################################################
+  # Another solution from my student: Jillian Davis 202020
+  #author: Jillian Davis
+  # dated: 17/09/2020
+  ##################################################################
+  
+ string = "the quick brown fox jumps over the lazy dog".split()
+unique_words = []
+word_counts = []
+for word in string:
+  if word not in unique_words:
+    unique_words.append(word)
+    word_counts.append(1)
+  else:
+    word_counts[unique_words.index(word)] += 1
+for i in range(len(unique_words)):
+  print(unique_words[i], word_counts[i])
