7bfdbdee9c61b851223d4c04b0ff76ecce096b58
akjalbani <akjalbani@yahoo.com>
2020-10-03 19:11:36 +1000

    Update Session10_ex2.py

--- Session10/Session10_ex2.py
+++ Session10/Session10_ex2.py
@@ -1,13 +1,13 @@
 '''Write a Python class which has two methods get_String and print_String. get_String accept a string from the user and print_String print the string in upper case.'''
 
 class ChangeCase:
-  def getString(self):
+  def get_String(self):
     self.msg = input("Enter your message:-> ")
     return self.msg
-  def convert_To_UpperCase(self,msg):
-    return self.msg.upper()
+  def print_String(self,msg):
+     print(self.msg.upper())
 
 obj = ChangeCase()
-user_input =obj.getString()
+user_input =obj.get_String()
 print(user_input)
-print(obj.convert_To_UpperCase(user_input))
+obj.print_String(user_input)
