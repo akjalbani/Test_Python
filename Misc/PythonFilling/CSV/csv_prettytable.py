44c898110c41892dc4bc1e497383d95b880231ce
akjalbani <akjalbani@yahoo.com>
2021-02-24 23:13:46 +1100

    Update csv_prettytable.py

--- Misc/PythonFilling/CSV/csv_prettytable.py
+++ Misc/PythonFilling/CSV/csv_prettytable.py
@@ -4,7 +4,16 @@
 # @ AA Jalbani
 # Date 24/02/2021 
 # Version 1.0
-
+##############################
+# one.csv file ( save this cv file)
+name,department,birthday_month
+John Smith,Accounting,November
+Erica Meyers,IT,March
+Vinay Michael,IT,April
+Max,IT,March
+################################
+# python file
+################################
 from prettytable import from_csv
 with open("one.csv",'r') as f:
   reader = from_csv(f)
