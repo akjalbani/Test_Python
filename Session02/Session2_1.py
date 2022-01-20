b599f26e6604fb98a33c325b97ee8fc4ee86c08a
akjalbani <akjalbani@yahoo.com>
2022-01-20 12:57:50 +1100

    Delete Session02 directory
    d s2

--- Session02/Session2_1.py
+++
@@ -1,7 +0,0 @@
-# Fahrenheit to celsius
-# c = (f-32) * 5 / 9
-f = 20
-c = (f-32) * 5 / 9
-print("Convert Fahrenheit into celsius ")
-print("Fahrenheit = ",f,"to celsius = ",c)
-
--- Session02/Session2_2.py
+++
@@ -1,6 +0,0 @@
-#: area = 0.5 * base * heigt
-length = float(input("Enter length of triangle: "))
-height = float(input("Enter height of triangle: "))
-area_of_triangle= 0.5 * length * height
-print("Area of Triangle = ", area_of_triangle)
-
