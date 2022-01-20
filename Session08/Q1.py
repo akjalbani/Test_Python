839f84105da6b7b66bc02fc7da9f1349645d0223
akjalbani <akjalbani@yahoo.com>
2022-01-20 12:59:20 +1100

    Delete Session08 directory
    d s8

--- Session08/Q1.py
+++
@@ -1,6 +0,0 @@
-# Python program to get the 4th element and 4th element from last of a tuple
-t = ("w","a","3","e","p","o","u","b","a","e")
-# 4th element 
-print(t[3])
-# from last
-print(t[-4])
--- Session08/Q2.py
+++
@@ -1,4 +0,0 @@
-#Q2 Convert the following list to tuple
-L= [1,2,3,4]
-tup = tuple(L)
-print(tup)
