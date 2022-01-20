ac7985463513ab38757c1d813889372db2aa8d42
akjalbani <akjalbani@yahoo.com>
2022-01-20 12:57:30 +1100

    Delete Session01 directory
    d s1

--- Session01/Session1_2.py
+++
@@ -1,6 +0,0 @@
-#Q2. Write a script that reads two numbers and prints #their sum.
-####################################################
-a = int(input("a -> "))
-b = int(input("b -> "))
-print(a+b)
-
--- Session01/Session1_3.py
+++
@@ -1,7 +0,0 @@
-#Q3. extend q2 and print sum of three numbers
-####################################################
-a = int(input("a -> "))
-b = int(input("b -> "))
-c = int(input("c -> "))
-print(a+b+c)
-
