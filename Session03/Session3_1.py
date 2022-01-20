4ff0e4f2f7bb019dfeed502518bfed3374a54c50
akjalbani <akjalbani@yahoo.com>
2022-01-20 12:58:08 +1100

    Delete Session03 directory
    d s3

--- Session03/Session3_1.py
+++
@@ -1,7 +0,0 @@
-'''1.	 The run time of a match is 105 minutes. Write a python program to calculate in hours and print the result. (understanding and using the floor division operator //). '''
-run_time = 105 # minute
-hr = 60 # 1 hr = 60 minutes
-tot_hrs = run_time//hr
-tot_minutes = run_time% hr
-
-print("Match was played for {} hr and {} minutes".format(tot_hrs,tot_minutes))
